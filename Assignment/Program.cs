f0def675a2f3408d099c6840ff79992ff01a7e72
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-07-25 14:39:10 +0300

    first

---
+++ Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.The Bubble Sort algorithm has a time complexity of O(n ^ 2) in its worst and average cases,
+            //  which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm
+            //  And implement the code of this optimized bubble sort algorithm
+
+        }
+    }
+}
