cfe088846637d6f73893df9581a4dd50ef3228be
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-07-25 11:14:30 +0300

    part1

---
+++ advanced-C#/Generics/Pono.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advanced_C_.Generics
+{
+    internal class Pono : IComparable<Pono>
+    {
+        public int X {  get; set; } 
+
+        public int Y { get; set; }
+        public Pono(int x, int y) 
+        {
+        X = x;
+        Y = y;
+        }
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+        public int CompareTo(Pono? other)
+        {
+            if (other is null)
+                return 1;
+            if (X == other.X)
+                return Y.CompareTo(other.Y);
+            else
+                return X.CompareTo(other.X);
+        }
+    }
+}
--- advanced-C#/Program.cs
+++ advanced-C#/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace advanced_C_
+﻿using advanced_C_.Generics;
+using System.Drawing;
+
+namespace advanced_C_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            int[] numbers = { 10, 3, 7, 1, 9, 12, 22, 19, 4 };
+
+            foreach (int number in numbers)
+            {
+                Console.Write($"{number} ");
+            }
+            Console.WriteLine("\n");
+            Helper<int>.BubbleSort( numbers );
+            foreach (int number in numbers)
+            {
+                Console.Write($"{number} ");
+            }
+            Console.WriteLine("\n");
+            Pono[] points =
+                {
+                new Pono(5,8), 
+                new Pono(10,9),
+                new Pono(1,3),
+                new Pono(1,7),
+                new Pono(15,4),
+                new Pono(2,7),
+                new Pono(12,9)
+                };
+            foreach (Pono point in points)
+            {
+                Console.WriteLine(point.ToString());
+            }
+            Console.WriteLine("\n");
+
+            Helper<Pono>.BubbleSort(points);
+            foreach (Pono point in points)
+            {
+                Console.WriteLine(point.ToString());
+            }
+            Console.WriteLine("\n");
+
+            int index = Helper<int>.SearchArray(numbers, 9);
+            Console.WriteLine(index.ToString());    
+
         }
     }
 }
